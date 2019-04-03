81022d50d2022488b6308b7fa6208df9409d8780
Ana <ana.fandino@outlook.com>
2019-04-02 22:43:19 -0500

    retos

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {FormattedDate, FormattedMessage, FormattedRelative, FormattedNumber, FormattedPlural} from 'react-intl';
 
 export default class Job extends React.Component {
 
@@ -6,11 +7,30 @@ export default class Job extends React.Component {
   		return (
   			<tr>
   				<th scope="row">{this.props.offer.id}</th>
-      			<td>{this.props.offer.name}</td>
-  				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td>
+					<FormattedMessage id={this.props.offer.name} />
+				</td>
+  				<td>
+					<FormattedMessage id={this.props.offer.company} />
+  				</td>
+      			<td>
+					{this.props.offer.salary} <FormattedPlural value={this.props.offer.salary} one={<FormattedMessage id="million" />} other={<FormattedMessage id="millions" />} />
+      			</td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+				<td>
+					<FormattedDate
+						value={new Date(this.props.offer.date)}
+						year='numeric'
+						month='long'
+						day='numeric'
+						weekday='long'
+					/> (<FormattedRelative value={new Date(this.props.offer.date)} format="short" />)
+				</td>
+				<td>
+					<FormattedNumber
+						value={this.props.offer.visits}
+					/>
+				</td>
   			</tr>
   		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider, addLocaleData} from 'react-intl';
+import esLocaleData from 'react-intl/locale-data/es';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
+
 
 import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+addLocaleData(esLocaleData);
+
+var lang = navigator.language || navigator.userLanguage;
+console.log(lang)
+
+ReactDOM.render(
+(lang === 'en-US')?(<IntlProvider locale="en" messages= {localeEnMessages}>
+        <JobsList/>
+    </IntlProvider>):(<IntlProvider locale="es" messages= {localeEsMessages}>
+    <JobsList/>
+</IntlProvider>), document.getElementById("root")
+);
